03d9d2df5e7c6e526b0c2e6d4603a828d00f7b47
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-08-27 19:02:39 -0300

    just some tests with enums

--- Game/index.c
+++ Game/index.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+enum State { Working = 1, Failed = 0 };
+
 int main() {
-  printf("Game");
+  enum State currentStatus = Working;
+
+  printf("Current state: %d", currentStatus);
   return 0;
 }
